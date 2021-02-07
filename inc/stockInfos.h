b537775e62f3cd789d98badea9c4ed5ea55330d7
Alexandre Malki <alexandremalki@gmail.com>
2021-02-07 15:56:16 +0100

    updating the tool

---
+++ inc/stockInfos.h
@@ -0,0 +1,22 @@
+#ifndef STOCK_INFO_H
+#define STOCK_INFO_H
+
+#include <QObject>
+#include <QString>
+
+class HistoryStock {
+
+};
+
+class StockInfo {
+public:
+    stockInfo(const QString &Name) {}
+    ~StockInfo {}
+
+private:
+   const QString mStockName;
+
+
+};
+
+#endif //STOCK_INFO_H
